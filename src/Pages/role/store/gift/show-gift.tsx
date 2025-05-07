72e9a2360ba0c0f4b437124b635f12c914d9f633
Nguyễn Minh Quân <quanquan2411@gmail.com>
2025-05-07 12:47:35 +0700

    update text

--- src/Pages/role/store/gift/show-gift.tsx
+++ src/Pages/role/store/gift/show-gift.tsx
@@ -513,7 +513,7 @@ export default function ShowGift() {
       </button>
 
       <h2 className="text-center text-xl font-light text-gray-700 mb-4 tracking-wider">
-        Nhập mã nhận sách
+        Nhập mã nhận quà
       </h2>
 
       <input
